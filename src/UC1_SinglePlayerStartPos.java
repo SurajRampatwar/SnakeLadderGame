b489d6e42472d13f765f23cceea0cd9ef5f0b86a
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-17 17:56:15 +0530

    Player with start position

---
+++ src/UC1_SinglePlayerStartPos.java
@@ -0,0 +1,10 @@
+public class UC1_SinglePlayerStartPos {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        int currPosition = 0;
+        int DiceValue = 0;
+        int PlayerPosition = 0;
+        System.out.println("Player at start position = " +currPosition);
+    }
+
+}
