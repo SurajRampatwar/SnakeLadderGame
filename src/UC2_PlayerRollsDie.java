37317066344b11a2cc88f888385c46f2ab343e47
SurajRampatwar <surajrampatwar123@gmail.com>
2022-05-17 18:45:36 +0530

    Player rolls Dice to get number

---
+++ src/UC2_PlayerRollsDie.java
@@ -0,0 +1,13 @@
+public class UC2_PlayerRollsDie {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder game");
+        int currPosition = 0;
+        int DiceValue = 0;
+        int PlayerPosition = 0;
+        System.out.println("Player at start position = " + currPosition);
+        DiceValue = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("Number of the rolled dice: " + DiceValue);
+        PlayerPosition = DiceValue;
+        System.out.println("Now player position is = "+PlayerPosition);
+    }
+}
